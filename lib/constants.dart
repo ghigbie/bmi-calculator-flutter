628bb51f00d03250e6c2a4b6cdfe68f96fc9bf05
ghigbie <gehigbie@gmail.com>
2019-06-23 16:39:55 -0600

    Edit var types

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
-const APP_TITLE = 'BMI Calculator';
+const String APP_TITLE = 'BMI Calculator';
 const CARD_COLOR_DEFAULT = 0xFF1D1E33;
 const CARD_COLOR_SELECTED = null;
-const CARD_MARGIN = 15.0;
-const CARD_RADIUS = 10.0;
+const double CARD_MARGIN = 15.0;
+const double ARD_RADIUS = 10.0;
