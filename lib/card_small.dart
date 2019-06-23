c7ba1824f93ea11a3b177414974ccd7f0a372459
ghigbie <gehigbie@gmail.com>
2019-06-23 15:24:46 -0600

    Add color to container

---
+++ lib/card_small.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class CardSmall extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.all(CARD_MARGIN),
+        decoration: BoxDecoration(
+          color: Color(0xFF1D1E33), //color must be added to box doraction 
+          borderRadius: BorderRadius.circular(CARD_RADIUS)
+        ),
+      ),
+    );
+  }
+}
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,1 +1,3 @@
 const APP_TITLE = 'BMI Calculator';
+const CARD_MARGIN = 15.0;
+const CARD_RADIUS = 10.0;
