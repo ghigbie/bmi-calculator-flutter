7782bac8d9a391661f284cb6ff82ac8b56410539
ghigbie <gehigbie@gmail.com>
2019-06-25 21:25:04 -0600

    Rename constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,17 +5,17 @@ const String kAppTitle = 'BMI Calculator';
 const String kcalculate = 'CALCULATE';
 const String kmale = 'MALE';
 const String kFemale = 'FEMALE';
-const ACTIVE_CARD_COLOR = Color(0xFF1D1E33);
-const INACTIVE_CARD_COLOR = Color(0xFF111328);
-const BOTTOM_CONTAINER_COLOR = Color(0xFFEB1555);
-const double CARD_MARGIN = 15.0;
-const double CARD_RADIUS = 10.0;
-const double BOTTOM_CARD_MARGIN = 10.0;
-const double BOTTOM_CONTAINER_HEIGHT = 80.0;
-const double ICON_SIZE = 80.0;
-const IconData MALE_ICON = FontAwesomeIcons.mars;
-const IconData FEMALE_ICON = FontAwesomeIcons.venus;
-const LABEL_TEXT_STYLE = TextStyle(
+const kActiveCarcColor = Color(0xFF1D1E33);
+const kInactiveCardColor = Color(0xFF111328);
+const kBottomContainerColor = Color(0xFFEB1555);
+const double kCardMargin = 15.0;
+const double kCardRadius = 10.0;
+const double kBottomCardMargin = 10.0;
+const double kBottomContainerHeight = 80.0;
+const double kIconSize = 80.0;
+const IconData kMaleIcon = FontAwesomeIcons.mars;
+const IconData kFemaleIcon = FontAwesomeIcons.venus;
+const kLableTextStyle = TextStyle(
   fontSize: 18.0,
   color: Color(0xFF8D8E98)
 );
