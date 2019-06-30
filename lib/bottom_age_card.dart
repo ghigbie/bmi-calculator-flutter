188c07fa80d71fc26502e46427e7053ba53fca5f
ghigbie <gehigbie@gmail.com>
2019-06-29 21:21:28 -0400

    Add bottom age card class

--- lib/bottom_age_card.dart
+++ lib/bottom_age_card.dart
@@ -2,48 +2,36 @@ import 'package:flutter/material.dart';
 import 'card_main.dart';
 import 'constants.dart';
 
-class BottomWeightCard extends StatefulWidget {
+class BottomAgeCard extends StatefulWidget {
   @override
-  _BottomWeightCardState createState() => _BottomWeightCardState();
+  _BottomAgeCardState createState() => _BottomAgeCardState();
 }
 
-int weight = 60;
+int age = 27;
 
-class _BottomWeightCardState extends State<BottomWeightCard> {
+class _BottomAgeCardState extends State<BottomAgeCard> {
   @override
   Widget build(BuildContext context) {
     return CardMain(
       colour: kActiveCardColor,
       cardChild: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Text(
-            'WEIGHT',
+            'AGE',
             style: kLableTextStyle
           ),
+          Text(
+            age.toString(),
+            style: kLableTextStyle,
+              ),
           Row(
             children: <Widget>[
-              Text(
-                weight.toString(),
-                style: kLargeTextStyle,
-              ),
-              Text(
-                'kg',
-                style: kLableTextStyle
-              )
+
             ],
           ),
-          Row(
-            children: <Widget>[
-              FlatButton(
-                child: Text(
-                  '-',
-                  style: kLargeTextStyle
-                  ),
-              )
-            ],
-          )
-        ],
+          ],
       ),
     );
   }
