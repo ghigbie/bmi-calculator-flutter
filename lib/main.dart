7d61fa82aa6674c15ca0bb4e2c786e8d2a9fa57c
George Higbie <ghigbie@monigle.com>
2019-07-04 13:40:29 -0600

    Change routing from named routes

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,7 @@ class BMICalculator extends StatelessWidget {
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0AE21)
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => InputPage(),
-        '/results': (context) => ResultsPage(),
-      }
+      home: InputPage(),
     );
   }
 }
