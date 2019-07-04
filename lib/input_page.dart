46c3f1d6112172e9b5218d11970bb4a9aa11dba5
George Higbie <ghigbie@monigle.com>
2019-07-03 20:22:30 -0600

    Add navigation

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -120,7 +120,11 @@ class _InputPageState extends State<InputPage> {
                 ],
               ),
             ),
-            BottomButton(),
+            BottomButton(
+              onPressed: (){
+                Navigator.pushNamed(context, '/results');
+              }
+            ),
           ],
         ),
       )
