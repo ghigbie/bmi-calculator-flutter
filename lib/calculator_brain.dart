6e84c930caf91d5c71f700aa76d89c957b2e1035
George Higbie <ghigbie@monigle.com>
2019-07-04 13:00:52 -0600

    Add get result method

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -1,11 +1,27 @@
+import 'dart:math';
+
 class CalculatorBrain {
 
   CalculatorBrain({this.height, this.weight});
-  
+
   final int height;
   final int weight;
+  double _bmi;
 
   String calculateBMI(){
-
+    double _bmi = weight / pow(height/100, 2); //height needs to be converted from cm to meters and thus is divided by 100
+    return _bmi.toStringAsFixed(1);
   }
+
+  String getResult(){
+    if(_bmi > 25){
+      return 'Overweight';
+    }else if(_bmi >= 18.5){
+      return 'Normal';
+    }else{
+      return 'Underwieght';
+    }
+  }
+
+
 }
